dd2cf2ac7e2bf9832533dfecc00dc463368e4bae
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-09-30 19:13:23 +0530

    fixed Loggers errors

--- AnieXEricaMusic/utils/logger.py
+++ AnieXEricaMusic/utils/logger.py
@@ -1,25 +1,23 @@
 from pyrogram.enums import ParseMode
-
 from AnieXEricaMusic import app
 from AnieXEricaMusic.utils.database import is_on_off
 from config import LOG_GROUP_ID
 
 
-async def play_logs(message, streamtype):
+async def LOG_GROUP_ID(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
-
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
-
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
-
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+╔════❰𝐏𝐋𝐀𝐘𝐈𝐍𝐆❱═══❍⊱❁۪۪
+<b>◈ 𝐂𝐡𝐚𝐭 ➪ </b>{message.chat.title}
+<b>◈ 𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ </b> <code>{message.chat.id}</code>
+<b>◈ 𝐔𝐬𝐞𝐫 ➪ </b> {message.from_user.mention}
+<b>◈ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➪ </b> @{message.from_user.username}
+<b>◈ 𝐈𝐝 ➪ </b> <code>{message.from_user.id}</code>
+<b>◈ 𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤 ➪ </b> @{message.chat.username}
+<b>◈ 𝐒𝐞𝐚𝐫𝐜𝐡𝐞𝐝 ➪ </b> <code>{message.text.split(None, 1)[1]}</code>
+<b>◈ 𝐁𝐲 ➪ </b> {streamtype}
+╚═══❰ #𝐍𝐞𝐰𝐒𝐨𝐧𝐠 ❱══❍⊱❁۪۪"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
