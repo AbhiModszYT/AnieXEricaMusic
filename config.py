24953fc80b8a1c8e8fc94807fba38832746325e0
NKMODNK6 <chotababbu654@gmail.com>
2024-09-30 08:10:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ GIT_TOKEN = getenv(
 )  
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Resso_music_group")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Hindi_English_singing_group_chat")
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
