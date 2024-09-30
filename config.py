bef54e68d3b354b815c004b65fa287f08996fccf
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-09-30 18:58:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
-LOGGER_ID = int(getenv("LOGGER_ID", -1001971806089))
+LOGGER_ID = int(getenv("LOGGER_ID",-1001971806089))
 
 OWNER_ID = int(getenv("OWNER_ID", 7045191057))
 
