88e03c38630ef11c82f61e0e63e2f2ebceb86166
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-10-05 22:27:47 +0530

    Create gcast.py

---
+++ AnieXEricaMusic/plugins/vips/gcast.py
@@ -0,0 +1,73 @@
+import random
+import asyncio
+from datetime import date
+from typing import Dict, List, Union
+from AnieXEricaMusic import userbot, app
+from AnieXEricaMusic.core.mongo import mongodb
+from pyrogram import Client, enums, filters
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant, ChatAdminRequired
+from pyrogram.types import Message, ChatPrivileges
+from typing import Optional
+from random import randint
+from datetime import datetime, timedelta
+from AnieXEricaMusic.misc import SUDOERS
+from AnieXEricaMusic.utils.database import (
+    get_active_chats,
+    get_authuser_names,
+    get_client,
+    get_served_chats,
+    get_served_users,
+)
+
+pros = mongodb.pro
+
+async def add_pro_user(user_id: int, duration: int):
+    expiration_date = datetime.now() + timedelta(days=duration)
+    await pros.update_one(
+        {'user_id': user_id},
+        {'$set': {'expires': expiration_date}}, 
+        upsert=True  
+    )
+
+async def is_pro(user_id: int) -> bool:
+    user = await pros.find_one({'user_id': user_id})
+    if user:
+        expires = user.get('expires')
+        if expires and expires > datetime.now():  
+            return True
+    return False
+
+async def extract_user(message: Message) -> Optional[Message]:
+    if message.reply_to_message:  
+        return message.reply_to_message.from_user
+    elif len(message.command) >= 2: 
+        AMBOT = message.command[1]
+        user = await app.get_users(AMBOT)
+        return user
+    else:
+        return None
+
+
+@app.on_message(filters.command("addpro") & SUDOERS)
+async def addpro_handler(client: Client, message: Message):
+    if not message.reply_to_message and len(message.command) < 2:
+        return await message.reply_text("Provide a user ID or reply to a message to add a user as pro.")
+    user = await extract_user(message)
+    if not user:
+        return await message.reply_text("Could not find the user.")
+    if user.id == message.from_user.id:
+        return await message.reply_text("You cannot add yourself to pro.")
+    elif user.id == app.id:
+        return await message.reply_text("You cannot add the bot to pro.")
+    if len(message.command) >= 3:
+        try:
+            duration = int(message.command[2]) 
+        except ValueError:
+            return await message.reply_text("Invalid duration. Please provide the number of days.")
+    else:
+        duration = 7 
+    is_pro_user = await is_pro(user.id)
+    if is_pro_user:
+        return await message.reply_text(f"{user.mention} is already a pro user.")
+    await add_pro_user(user.id, duration)
+    return await message.reply_text(f"{user.mention} has been added to pro for {duration} days.")
