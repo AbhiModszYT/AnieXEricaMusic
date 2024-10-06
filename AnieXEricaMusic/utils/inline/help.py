82799f858151e80e5971a2a3c41283c64287cd6a
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-10-06 11:17:14 +0530

    Update help.py

--- AnieXEricaMusic/utils/inline/help.py
+++ AnieXEricaMusic/utils/inline/help.py
@@ -95,6 +95,10 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_17"],
                     callback_data="help_callback hb17",
                 ),
+                InlineKeyboardButton(
+                    text=_["H_B_18"],
+                    callback_data="back_to_privacy",
+                ),
             ],
             mark,
         ]
