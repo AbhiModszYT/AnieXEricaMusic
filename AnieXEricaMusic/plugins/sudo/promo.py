a28f05a271b5cd8bd4cb700f9d2fff697d98cd69
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-10-10 01:03:20 +0530

    Create promo.py

---
+++ AnieXEricaMusic/plugins/sudo/promo.py
@@ -0,0 +1,33 @@
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from AnieXEricaMusic import app
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message, User
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden, PeerIdInvalid, ChatAdminRequired
+from pyrogram.enums import ChatAction, ChatType, MessageEntityType
+from pyrogram import Client, filters, enums
+from AnieXEricaMusic.misc import SUDOERS
+
+buttons = [
+    [
+        InlineKeyboardButton(
+            text="➕ ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ", 
+            url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users"
+        ),
+    ],
+]
+
+@app.on_message(filters.command(["promo"]) & SUDOERS)
+async def promos(client, message: Message):
+    BANCODES = f"""{app.mention},
+🤖 ᴀᴅᴠᴀɴᴄᴇᴅ ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘ ᴠɪᴅᴇᴏᴄʜᴀᴛs 🤖
+⚡️ғᴇᴀᴛᴜʀᴇs ⚡️
+➻ ɪ ᴄᴀɴ ᴘʟᴀʏ ꜱᴏɴɢ ɪɴ ɢʀᴏᴜᴘ ᴠᴄ.
+➻ ɴᴏ ʟᴀɢ.
+➻ ʙᴇꜱᴛ ꜱᴏᴜɴᴅ Qᴜᴀʟɪᴛʏ.
+➻ 24×7 ᴜᴘᴛɪᴍᴇ.
+➻ ʟᴀɢ ғʀᴇᴇ.
+"""
+    await message.reply(
+        text=BANCODES,
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
