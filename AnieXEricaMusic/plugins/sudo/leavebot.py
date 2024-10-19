96fa8623ce041f14d8de0e4befbee0b2a5ca450e
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-10-19 13:37:46 +0530

    Create leavebot.py

---
+++ AnieXEricaMusic/plugins/sudo/leavebot.py
@@ -0,0 +1,23 @@
+from AnieXEricaMusic import app 
+from pyrogram import filters
+from pyrogram.enums import ChatMemberStatus
+import asyncio
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
+from AnieXEricaMusic.misc import SUDOERS
+
+@app.on_message(filters.command("leave") & SUDOERS)
+async def leave(_, message):
+    if len(message.command) != 2:
+        return await message.reply_text("Please provide a group ID. Use like: `/leave chat_id`.")
+    try:
+        chat_id = int(message.command[1])
+    except ValueError:
+        return await message.reply_text(f"Invalid chat ID. Please enter a numeric ID.")
+    SKY = await message.reply_text(f"Leaving chat... {ECOMUSIC.me.mention}")
+    try:
+        await app.send_message(chat_id, f"{ECOMUSIC.me.mention} Lefting chat Bye...")
+        await app.leave_chat(chat_id)
+        await SKY.edit(f"{ECOMUSIC.me.mention} Left chat {chat_id}.")
+    except Exception as e:
+        pass
